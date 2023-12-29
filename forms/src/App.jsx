747dd03d2bb7ff03b5bb88b5f251d517f54cf17b
rajashree-guha <rajashree.625.guha@gmail.com>
2023-12-29 09:18:58 +0530

    done

---
+++ forms/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Forms from './components/Forms'
+import './App.css'
+import FormUse from './components/FormUse'
+
+function App() {
+
+  return (
+    <div>
+      {/* <Forms/> */}
+      <FormUse/>
+    </div>
+  )
+}
+
+export default App
